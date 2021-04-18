edc2628d756514e671e4d2d50817a1e1c27ba553
fosterion <32929118+fosterion@users.noreply.github.com>
2021-04-18 14:58:49 +0300

    Add project files.

---
+++ src/Evercell/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evercell
+{
+    class MainViewModel
+    {
+        public string ButtonText { get; private set; }
+
+        public MainViewModel()
+        {
+            ButtonText = "This is the button text";
+        }
+    }
+}
