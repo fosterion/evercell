0470300ccb0b6391d4b2deda44fe5447976436a0
fosterion <32929118+fosterion@users.noreply.github.com>
2021-04-22 00:50:41 +0300

    Add views to main window as context.

---
+++ src/Evercell/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Evercell.Core;
+using Evercell.Mvvm.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Evercell
+{
+    class MainViewModel : ObservableObject
+    {
+        public ICommand SwitchToHomeCommand => new RelayCommand(() => SwitchTo(Home), () => true);
+
+        public ICommand SwitchToSimulationCommand => new RelayCommand(() => SwitchTo(Simulation), () => true);
+
+        public HomeViewModel Home { get; private set; }
+
+        public SimulationViewModel Simulation { get; private set; }
+
+        public object CurrentContext
+        {
+            get => GetValue<object>(nameof(CurrentContext));
+            private set => SetValue(nameof(CurrentContext), value);
+        }
+
+        public MainViewModel()
+        {
+            Home = new HomeViewModel();
+            Simulation = new SimulationViewModel();
+
+            CurrentContext = Home;
+        }
+
+        private void SwitchTo(IContext context)
+        {
+            if (CurrentContext != context)
+            {
+                CurrentContext = context;
+            }
+        }
+    }
+}
