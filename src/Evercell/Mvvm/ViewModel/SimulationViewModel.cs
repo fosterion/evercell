0eb7b9c42678b6f8ba4aa3ff22d20544d4e345bd
fosterion <32929118+fosterion@users.noreply.github.com>
2021-05-06 22:10:13 +0300

    Trying to binding bitmap.

--- src/Evercell/Mvvm/ViewModel/SimulationViewModel.cs
+++ src/Evercell/Mvvm/ViewModel/SimulationViewModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Evercell.Core;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace Evercell.Mvvm.ViewModel
 {
-    class SimulationViewModel : IContext
+    class SimulationViewModel : ObservableObject, IContext
     {
+        private Graphics graphics;
 
+        public ICommand NextStep => new RelayCommand(CreateBitmap);
+
+        public Bitmap ImageSource
+        {
+            get => GetValue<Bitmap>(nameof(ImageSource));
+            private set => SetValue(nameof(ImageSource), value);
+        }
+
+        public SimulationViewModel()
+        {
+
+        }
+
+        private void CreateBitmap()
+        {
+            ImageSource = new Bitmap(500, 500);
+            graphics = Graphics.FromImage(ImageSource);
+            graphics.FillRectangle(Brushes.Red, 0, 0, 250, 250);
+        }
     }
 }
