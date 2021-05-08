3e35c8e3000e69547e6db2015cd4d7ae63769695
fosterion <32929118+fosterion@users.noreply.github.com>
2021-05-08 23:23:15 +0300

    Add new cell class.

---
+++ src/Evercell/Mvvm/Model/Cell.cs
@@ -0,0 +1,17 @@
+﻿using Evercell.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Evercell.Mvvm.Model
+{
+    class Cell
+    {
+        public Brush Color { get; set; }
+
+        public bool IsAlive { get; set; }
+    }
+}
--- src/Evercell/Mvvm/ViewModel/SimulationViewModel.cs
+++ src/Evercell/Mvvm/ViewModel/SimulationViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Evercell.Core;
 using Evercell.Interfaces;
+using Evercell.Mvvm.Model;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,65 +18,20 @@ namespace Evercell.Mvvm.ViewModel
 {
     class SimulationViewModel : ObservableObject, IContext
     {
-        private Graphics graphics;
-
-        public ICommand NextStep => new RelayCommand(CreateBitmap);
-
-        public BitmapImage ImageSource
-        {
-            get => GetValue<BitmapImage>();
-            private set => SetValue(value);
-        }
+        public Cell[,] Source;
 
         public SimulationViewModel()
         {
+            Source = new Cell[50, 50];
 
-        }
-
-        private void CreateBitmap()
-        {
-            ImageSource = new BitmapImage(new Uri(@"", UriKind.Absolute));
-            var src = BitmapImage2Bitmap(ImageSource);
-            graphics = Graphics.FromImage(src);
-            graphics.FillRectangle(Brushes.Red, 0, 0, 250, 250);
-            ImageSource = Bitmap2BitmapImage(src);
-        }
-
-        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
-        {
-            using (MemoryStream outStream = new MemoryStream())
+            for (int i = 0; i < Source.Length / 50; i++)
             {
-                BitmapEncoder enc = new BmpBitmapEncoder();
-                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
-                enc.Save(outStream);
-                Bitmap bitmap = new Bitmap(outStream);
-
-                return new Bitmap(bitmap);
+                Source[i, i] = new Cell()
+                {
+                    Color = Brushes.Aquamarine,
+                    IsAlive = true
+                };
             }
-        }
-
-        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
-        public static extern bool DeleteObject(IntPtr hObject);
-
-        private BitmapImage Bitmap2BitmapImage(Bitmap bitmap)
-        {
-            IntPtr hBitmap = bitmap.GetHbitmap();
-            BitmapImage retval;
-
-            try
-            {
-                retval = (BitmapImage)Imaging.CreateBitmapSourceFromHBitmap(
-                             hBitmap,
-                             IntPtr.Zero,
-                             Int32Rect.Empty,
-                             BitmapSizeOptions.FromEmptyOptions());
-            }
-            finally
-            {
-                DeleteObject(hBitmap);
-            }
-
-            return retval;
         }
     }
 }
