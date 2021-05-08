bcb8f4af1f410f3857e6fe7d1cef9153908587b1
fosterion <32929118+fosterion@users.noreply.github.com>
2021-05-09 00:38:20 +0300

    Trying to bind 2d array to datagrid.

---
+++ src/Evercell/Converters/BooleanToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Evercell.Converters
+{
+    class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool boolean))
+            {
+                return Brushes.WhiteSmoke;
+            }
+            else
+            {
+                bool concrete = boolean;
+
+                if (concrete)
+                {
+                    return Brushes.YellowGreen;
+                }
+                else
+                {
+                    return Brushes.WhiteSmoke;
+                }
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Evercell/Mvvm/ViewModel/SimulationViewModel.cs
+++ src/Evercell/Mvvm/ViewModel/SimulationViewModel.cs
@@ -13,25 +13,72 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media.Imaging;
+using System.Data;
 
 namespace Evercell.Mvvm.ViewModel
 {
     class SimulationViewModel : ObservableObject, IContext
     {
-        public Cell[,] Source;
+        public Cell[,] Cells { get; set; }
+
+        public DataView BiCells { get; set; }
 
         public SimulationViewModel()
         {
-            Source = new Cell[50, 50];
+            Cells = new Cell[25, 25];
 
-            for (int i = 0; i < Source.Length / 50; i++)
+            for (int col = 0; col < 25; col++)
             {
-                Source[i, i] = new Cell()
+                for (int row = 0; row < 25; row++)
                 {
-                    Color = Brushes.Aquamarine,
-                    IsAlive = true
-                };
+                    var rnd = new Random().Next(0, 1);
+
+                    if (rnd == 0)
+                    {
+                        Cells[col, row] = new Cell(true);
+                    }
+                    else
+                    {
+                        Cells[col, row] = new Cell(false);
+                    }
+                }
             }
+
+            BiCells = GetBindable2DArray(Cells);
+        }
+
+        public static DataView GetBindable2DArray<T>(T[,] array)
+        {
+            var table = new DataTable();
+            for (var i = 0; i < array.GetLength(1); i++)
+            {
+                table.Columns.Add((i + 1).ToString(), typeof(bool))
+                             .ExtendedProperties.Add("idx", i); // Save original column index
+            }
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                table.Rows.Add(table.NewRow());
+            }
+
+            var view = new DataView(table);
+            for (var ri = 0; ri < array.GetLength(0); ri++)
+            {
+                for (var ci = 0; ci < array.GetLength(1); ci++)
+                {
+                    view[ri][ci] = array[ri, ci];
+                }
+            }
+
+            // Avoids writing an 'AutogeneratingColumn' handler
+            table.ColumnChanged += (s, e) =>
+            {
+                var ci = (int)e.Column.ExtendedProperties["idx"]; // Retrieve original column index
+                var ri = e.Row.Table.Rows.IndexOf(e.Row); // Retrieve row index
+
+                array[ri, ci] = (T)view[ri][ci];
+            };
+
+            return view;
         }
     }
 }
